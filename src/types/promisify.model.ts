a9c07db5b3f664d7f0912014695b77525611c2eb
Jeff Hage <ownerejeff@gmail.com>
2020-03-07 14:35:00 -0500

    Format

--- src/types/promisify.model.ts
+++ src/types/promisify.model.ts
@@ -1,7 +1,5 @@
 export type Promisify<Library, ReturnType> = {
-  [Method in keyof Library]: Library[Method] extends (
-    ...args: infer U
-  ) => infer ReturnValue
-    ? (...args: Partial<U>) => Promise<ReturnType>
-    : never;
+    [Method in keyof Library]: Library[Method] extends (...args: infer U) => infer ReturnValue
+        ? (...args: Partial<U>) => Promise<ReturnType>
+        : never;
 };
